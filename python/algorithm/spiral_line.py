9aefa7c828d4c1ff34349d0088d2dc2d2e0bd5bc
pan1fan2 <panfanubc@gmail.com>
2022-01-18 01:09:43 -0800

    update more practices

--- python/algorithm/spiral_line.py
+++ python/algorithm/spiral_line.py
@@ -1,10 +1,20 @@
 import turtle
+# from typing import ForwardRef
 t = turtle.Turtle()
-def drawSpiral(t,linelen):
-    if linelen > 0 :
-        t.forward(linelen)
-        t.right(90)
-        drawSpiral(t,linelen - 5)
+# def drawSpiral(t,linelen):
+#     if linelen > 0 :
+#         t.forward(linelen)
+#         t.right(90)
+#         drawSpiral(t,linelen - 5)
 
-drawSpiral(t,100)
+# drawSpiral(t,100)
+# turtle.done()
+
+
+for i in range(0,3):
+    t.forward(100)
+    t.left(120)
+    t.backward(100)
+    if i ==1:
+        break
 turtle.done()
